31f3a9fe8f0bfc1f993ed1c3c1535254b12482f5
PapaFreddie <fred.papa@student.moringaschool.com>
2021-12-01 14:35:06 +0300

    added paragraphs to body and updated style

--- script/app.js
+++ script/app.js
@@ -1,13 +1,18 @@
 //access data function
-const firstName = document.querySelector("#first-name");
-const surname = document.querySelector("#surname");
+const firstNameInput = document.querySelector("#first-name");
+const surnameInput = document.querySelector("#surname");
 const email = document.querySelector("#email");
 const telNumber = document.querySelector("#telnumber");
-const comments = document.querySelector("#comments-message");
+const message = document.querySelector("#comments-message");
 const submit = document.querySelector("#submit");
+const errorNodes = document.querySelectorAll(".error");
+const success = document.querySelector("#success")
 
 
 //data validation function
 function validateForm(){
-
+    if(firstNameInput.value.length < 1){
+        errorNodes[0].innerText = "Name cannot be blank"
+        firstNameInput.classList.add("error-border")
+    }
 }
