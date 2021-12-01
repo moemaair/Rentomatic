c32510408da06317261732c1cfaa92aa5497d563
yahyasaadi <yahyasaadi9219@gmail.com>
2021-12-01 15:25:15 +0300

    Added a function to check for inputs

--- script/script.js
+++ script/script.js
@@ -5,7 +5,43 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 
-
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+    // NOT WORKING AS EXPECTED
+    // return window.location.href="home.html";
+}
 
 
 
