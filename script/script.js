3aed0d0341ae0b551aebb568c87157b4215b4ca3
yahyasaadi <yahyasaadi9219@gmail.com>
2021-12-01 15:26:37 +0300

    Added a function to check if email is in correct format

--- script/script.js
+++ script/script.js
@@ -44,6 +44,10 @@ function checkInputs() {
 }
 
 
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
 
 
 $(document).ready(function(){
