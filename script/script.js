5b9a2637d48e6767978474836c17fceb7f91ef3f
yahyasaadi <yahyasaadi9219@gmail.com>
2021-12-01 15:23:11 +0300

    Added variables

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+
+
+
+
+
 $(document).ready(function(){
     $("#card").click(function(){
         $("#cardBody").toggle();
