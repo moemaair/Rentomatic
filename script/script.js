65f942f2bcf0dbcbcff872d6dc49a8cab8b2714c
yahyasaadi <yahyasaadi9219@gmail.com>
2021-12-01 14:34:19 +0300

    Added animation to the cards dive in home.html

--- script/script.js
+++ script/script.js
@@ -3,7 +3,12 @@ $(document).ready(function(){
         $("#cardBody").toggle();
     });
 
-    // $("img").click(function(){
-    //     $("div#cardBody").toggle();
-    // });
+    
+    $(".rentbuy-cards").each(function(){
+        $("img").hover(function(){
+            $(this).animate({opacity: .1});
+        }, function(){
+            $(this).animate({opacity: 1});
+        });
+    });
 });
