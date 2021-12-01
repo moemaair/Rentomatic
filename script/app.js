e771552d6fca6aa05b4e9bedf98610b1a0be33db
PapaFreddie <fred.papa@student.moringaschool.com>
2021-12-01 13:58:31 +0300

    update everything

---
+++ script/app.js
@@ -0,0 +1,13 @@
+//access data function
+const firstName = document.querySelector("#first-name");
+const surname = document.querySelector("#surname");
+const email = document.querySelector("#email");
+const telNumber = document.querySelector("#telnumber");
+const comments = document.querySelector("#comments-message");
+const submit = document.querySelector("#submit");
+
+
+//data validation function
+function validateForm(){
+
+}
