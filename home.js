2d0a4c425978e322ae30ff9c9e11d6a7943b257f
Mohamed A. Ibrahim <rageali12@gmail.com>
2021-12-02 14:15:41 +0300

    added changes on hero page for team-member

--- home.js
+++ home.js
@@ -1,5 +1,19 @@
-// var closebtns = document.getElementsByClassName("btn-close");
+var closebtns = document.querySelector(".btn-close");
+var p = document.querySelector(".ptag");
+
+var headingBanner = document.querySelector("#heading-banner");
+
 // var i;
+closebtns.addEventListener('click', ()=>{
+  headingBanner.style.display="none";
+
+  // headingBanner.style.display="none";
+ 
+})
+
+
+
+
 
 // for (i = 0; i < closebtns.length; i++) {
 //   closebtns[i].addEventListener("click", function() {
