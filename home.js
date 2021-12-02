1e0ee00be40d4f5f29b81417d76a536d3d7db4b8
Mohamed A. Ibrahim <rageali12@gmail.com>
2021-12-02 21:58:07 +0300

    added home banner toggle

--- home.js
+++ home.js
@@ -1,22 +1,6 @@
-var closebtns = document.querySelector(".btn-close");
-var p = document.querySelector(".ptag");
-
-var headingBanner = document.querySelector("#heading-banner");
-
-// var i;
-closebtns.addEventListener('click', ()=>{
-  headingBanner.style.display="none";
-
-  // headingBanner.style.display="none";
- 
+var headingBanner = document.getElementById('heading-banner')
+var headingClose = document.getElementById('btn-close')
+headingClose.addEventListener('click', (e) => {
+  headingBanner.classList.add("hide")
+  headingBanner.classList.remove("d-flex")
 })
-
-
-
-
-
-// for (i = 0; i < closebtns.length; i++) {
-//   closebtns[i].addEventListener("click", function() {
-//     this.parentElement.style.display = 'none';
-//   });
-// }
