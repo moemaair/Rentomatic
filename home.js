c648fdec41bd6829062db48f6f2a73e2381ba7ad
Mohamed A. Ibrahim <rageali12@gmail.com>
2021-12-03 07:16:23 +0300

    changed some cards address

--- home.js
+++ home.js
@@ -4,3 +4,10 @@ headingClose.addEventListener('click', (e) => {
   headingBanner.classList.add("hide")
   headingBanner.classList.remove("d-flex")
 })
+
+// inherited the mobile view toggle from script.js
+$().ready(function(){
+  $('#hamburgerIcon').click(function (){
+    $('.mobile-menu').toggle();
+  })
+})
---
+++ script/script.js
@@ -0,0 +1,6 @@
+// toogle on hamburger icon for rent and buy pages
+$().ready(function(){
+  $('#hamburgerIcon').click(function (){
+    $('.mobile-menu').toggle();
+  })
+})
